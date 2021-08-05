784e39fc51f82c12deee9c071a6b013ca0ab755f
Nemanja Budurin <n.budurin@gmail.com>
2021-08-05 14:12:44 +0200

    New Commit

--- app/app.js
+++ app/app.js
@@ -8,3 +8,22 @@ $(document).ready(function() {
 	};
 	$('.slider').slick(slickOpts);
   });
+
+
+$(document).ready(function() {
+	$("a#single_image").fancybox();
+
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
