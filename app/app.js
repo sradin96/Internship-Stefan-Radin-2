070c447ada19e5d760c1427e2fe46437f7269725
Nemanja Budurin <n.budurin@gmail.com>
2021-08-09 14:10:47 +0200

    Styles Commit

--- app/app.js
+++ app/app.js
@@ -42,21 +42,17 @@ $(document).ready(function(){
 	$(window).scroll(function(){
 		var scroll = $(window).scrollTop();
 		if (scroll > 60) {
-		  $(".top").css("transform" , "translateY(-100%)");
-		  $(".top").css("transition" , "all .1s ease-out");  
+		  $(".top").css({"transform": "translateY(-100%)" , "transition": "all .2s ease-out"});
 		}
   
 		else {
-			$(".top").css("transform" , "translateY(0%)");  
-			$(".top").css("transition" , "all .1s ease-out");  	
+			$(".top").css({"transform": "translateY(0%)" , "transition": "all .2s ease-out"});  	
 		}
 		if (scroll > 60) {
-			$(".bottom").css("transform" , "translateY(-50%)");
-			$(".bottom").css("transition" , "all .2s ease-out");
+			$(".bottom").css({"transform": "translateY(-50%)" , "transition": "all .2s ease-out"});
 		  }	
 		  else {
-			  $(".bottom").css("transform" , "translateY(0%)");  	
-			  $(".bottom").css("transition" , "all .2s ease-out");
+			  $(".bottom").css({"transform": "translateY(0%)" , "transition": "all .2s ease-out"});  	
 		  }
 	})
 })
